c4c5e64fe0765ce17e1f7e75583656da2368f2be
Alexander Krenz <krenzalexander@proton.me>
2025-08-04 10:25:51 +0200

    feat: move init grid code into initGrid function

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+const GRID_SIZE = 16;
+
 function onCellHover(event) {
     const target = event.target;
     target.classList.add("hovered");
 }
 
-const GRID_SIZE = 16;
-const container = document.querySelector(".container");
+function initGrid() {
+    const container = document.querySelector(".container");
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+    for (let i = 0; i < GRID_SIZE; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        i
+        for (let j = 0; j < GRID_SIZE; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", onCellHover);
+            row.appendChild(cell);
+        }
 
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener("mouseenter", onCellHover);
-        row.appendChild(cell);
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
